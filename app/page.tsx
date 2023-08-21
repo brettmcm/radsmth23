384b256306019ed57a2e3ad14f6649047df4baf5
Brett McMillin <brettmcm@me.com>
2023-08-21 03:46:09 +0000

    updated canary cover

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 
+type CaseType = {
+  title: string;
+  services: string;
+  cover: string;
+};
+
 export default function Home() {
   return (
     <div>
       <HeaderNav />
       <main>
-        <HeroBanner />
-        <CaseCover />
-        <CaseCover />
+        <HeroBanner />z
+        <CaseCover
+          title="Canary"
+          services="Branding, Visual Identity"
+          cover="/covers/canary-cover.png"
+        />
+        <CaseCover
+          title="Island Dwell"
+          services="Branding, Visual Identity"
+          cover="/covers/islanddwell-hero.jpg"
+        />
       </main>
     </div>
   )
@@ -32,20 +46,22 @@ export function HeroBanner() {
   )
 }
 
-export function CaseCover() {
+const CaseCover = (props: CaseType) => {
+
+  const { title, services, cover } = props;
 
   return (
     <div className="case-study">
       <div className="img">
         <Image
           fill
-          src="/covers/canary-05.png"
+          src={cover}
           alt="Canary branding image"
           className="imgimg"
         />
       </div>
-      <h2>Canary</h2>
-      <p>Branding, Visual Identity</p>
+      <h2>{title}</h2>
+      <p>{services}</p>
     </div>
   )
 }
