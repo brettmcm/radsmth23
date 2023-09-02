da205a46c3628ed38785d6967e50e4a4a3aedad4
Brett McMillin <brettmcm@me.com>
2023-09-01 21:31:11 -0700

    hid it

--- pages/casestudy.tsx
+++ pages/casestudy.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
-import './case-study.css'
+// import Image from 'next/image'
+// import './case-study.css'
 
-export default function CaseStudy() {
+// export default function CaseStudy() {
     
-    return (
-        <article>
-            <ul className="cs-meta">
-                <li><h3>Client</h3>Canary</li>
-                <li><h3>Category</h3>Consumer goods</li>
-                <li><h3>Credit</h3>Branding & visual identity</li>
-                <li><h3>Location</h3>Oceanside, CA</li>
-                <li><h3>Year</h3>2023</li>
-            </ul>
-            <h1>Canary Co.</h1>
-            <p>With no desire to join the green-washed eco-friendly aesthetic, Canary approached Radsmth with the challenge of bringing the company's more subversive, punk-rock ethos to the world of plastic-free, sustainable personal & home care products.</p>
-        </article>
-    )
-}
+//     return (
+//         <article>
+//             <ul className="cs-meta">
+//                 <li><h3>Client</h3>Canary</li>
+//                 <li><h3>Category</h3>Consumer goods</li>
+//                 <li><h3>Credit</h3>Branding & visual identity</li>
+//                 <li><h3>Location</h3>Oceanside, CA</li>
+//                 <li><h3>Year</h3>2023</li>
+//             </ul>
+//             <h1>Canary Co.</h1>
+//             <p>With no desire to join the green-washed eco-friendly aesthetic, Canary approached Radsmth with the challenge of bringing the company's more subversive, punk-rock ethos to the world of plastic-free, sustainable personal & home care products.</p>
+//         </article>
+//     )
+// }
