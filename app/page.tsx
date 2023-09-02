6d01e686a0140b8c701bb7b72c1aaec54b822462
Brett McMillin <brettmcm@me.com>
2023-09-01 21:27:41 -0700

    cleaned up stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Image from 'next/image'
-import { useRef } from 'react'
+import Link from 'next/link';
 import { motion, useScroll, useTransform, MotionValue } from "framer-motion"
+
+import CaseStudy from '../pages/casestudy';
 
 type CaseType = {
   title: string;
@@ -10,49 +12,6 @@ type CaseType = {
   cover: string;
 };
 
-export default function Home() {
-  return (
-    <div>
-      <HeaderNav />
-      <main>
-        <HeroBanner />
-        <div className="work">
-          <CaseCover
-            title="Canary"
-            services="Branding, Visual Identity"
-            cover="/covers/canary-cover.png"
-          />
-          <CaseCover
-            title="Island Dwell"
-            services="Branding, Visual Identity"
-            cover="/covers/islanddwell-hero.jpg"
-          />
-
-          <CaseCover
-            title="Apeak"
-            services="Brand Naming, Visual Identity"
-            cover="/covers/apeak.jpg"
-          />
-        </div>
-      </main>
-    </div>
-  )
-}
-
-function HeaderNav() {
-
-  const { scrollYProgress } = useScroll();
-  const y = useParallaxHeader(scrollYProgress, 100);
-
-  return (
-    <motion.header
-      style={{ y }}
-    >
-      <h1>Radsmth—</h1>
-      <a href="mailto:brett@radsmth.com">Contact</a>
-    </motion.header>
-  )
-}
 
 function useParallax(value: MotionValue<number>, distance: number) {
   return useTransform(value, [0, 1], [0, distance]);
@@ -64,21 +23,84 @@ function useZoom(value: MotionValue<number>, distance: number) {
   return useTransform(value, [0, 1], [1, distance]);
 }
 function useOpacity(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [1, distance]);
+  return useTransform(value, [0, 0.15], [1, distance]);
 }
+
+export default function Home() {
+
+  const { scrollYProgress } = useScroll();
+  const y = useParallax(scrollYProgress, -1500);
+  const o = useOpacity(scrollYProgress, 0);
+
+  return (
+    <div>
+      <HeaderNav />
+        <motion.div style={{ y, opacity: o }} className="headline">
+          <h1><div>Rad</div><div>smth</div></h1>
+        </motion.div>
+        <main>
+          <Link href="/casestudy">
+              <CaseCover
+              title="Canary"
+              services="Branding, Visual Identity"
+              cover="/covers/canary-cover.png"
+            />
+          </Link>
+          <CaseCover
+            title="Island Dwell"
+            services="Branding, Visual Identity"
+            cover="/covers/islanddwell-hero.jpg"
+          />
+
+          <CaseCover
+            title="Apeak"
+            services="Brand Naming, Visual Identity"
+            cover="/covers/apeak.jpg"
+          />
+        </main>
+      <HeroBanner />
+      {/* <CaseStudy /> */}
+    </div>
+  )
+}
+
+
+
+
+
+
+
+function HeaderNav() {
+
+  const { scrollYProgress } = useScroll();
+  const y = useParallaxHeader(scrollYProgress, 100);
+
+  return (
+    <motion.header style={{ y }} >
+      <h1>Radsmth—</h1>
+      <a href="mailto:brett@radsmth.com">Contact</a>
+    </motion.header>
+  )
+}
+
+
+
+
+
+
 
 function HeroBanner() {
 
   const { scrollYProgress } = useScroll();
-  const y = useParallax(scrollYProgress, 3000);
-  const s = useZoom(scrollYProgress, 3);
-  const o = useOpacity(scrollYProgress, 0);
+  const y = useParallax(scrollYProgress, 500);
+  const s = useZoom(scrollYProgress, 4);
+
+  const filter = useTransform(scrollYProgress, v => `brightnes(${v})`);
 
   return (
     <div className="hero-banner">
-        <motion.h1 style={{ opacity: o }}><div>Rad</div><div>smth</div></motion.h1>
         <motion.div
-          style={{ y, scale: s }}
+          style={{ y, scale: s, filter }}
           className="heroFGcontainer"
           whileInView="visible"
         >
@@ -88,9 +110,16 @@ function HeroBanner() {
           className="heroFG"
         />
         </motion.div>
+        <div className="viewfinder"></div>
     </div>
   )
 }
+
+
+
+
+
+
 
 const CaseCover = (props: CaseType) => {
 
@@ -98,16 +127,16 @@ const CaseCover = (props: CaseType) => {
 
   return (
     <div className="case-study">
-      <div className="img">
-        <Image
-          fill
-          src={cover}
-          alt="Canary branding image"
-          className="imgimg"
-        />
+      <Image
+        fill
+        src={cover}
+        alt="Canary branding image"
+        className="img"
+      />
+      <div className="meta">
+        <h2 data-replace={title}><span>{title}</span></h2>
+        <p>{services}</p>
       </div>
-      <h2>{title}</h2>
-      <p>{services}</p>
     </div>
   )
 }
