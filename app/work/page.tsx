5e118b719c0533160c78a2f23e4b2d6b2cd727b3
Brett McMillin <brettmcm@me.com>
2023-09-07 19:17:16 -0700

    other lint

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,17 +2,7 @@ export default function CaseStudy() {
     
     return (
         <div>
-            <article>
-                <ul className="cs-meta">
-                    <li><h3>Client</h3>Canary</li>
-                    <li><h3>Category</h3>Consumer goods</li>
-                    <li><h3>Credit</h3>Branding & visual identity</li>
-                    <li><h3>Location</h3>Oceanside, CA</li>
-                    <li><h3>Year</h3>2023</li>
-                </ul>
-                <h1>Canary Co.</h1>
-                <p>With no desire to join the green-washed eco-friendly aesthetic, Canary approached Radsmth with the challenge of bringing the company's more subversive, punk-rock ethos to the world of plastic-free, sustainable personal & home care products.</p>
-            </article>
+            Hello world
         </div>
     )
 }
